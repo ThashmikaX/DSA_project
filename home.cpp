cf3650e188a271616668629698fa11dc716a8e2f
viraj <vchampike@gmail.com>
2023-11-14 19:08:49 +0530

    update 14/11

--- home.cpp
+++ home.cpp
@@ -1,41 +1,37 @@
 #include "home.h"
-#include<iostream>
+#include <iostream>
+#include <limits> // for numeric_limits
 
 using namespace std;
 
-void home()
-{
-    
+void home() {
     int x;
-    cout << "1 Registration" << endl;
-    cout << "2 Log-in" << endl;
-    cout << "3 About us" << endl;
-    cout<<"Select the option number : ";
-    cin >>x;
-    while (x>0 || x<4)
-    {
-        
-        if(x==1){
-            cout << "Registration" << endl;
+    do{
+        cout << "1. Registration\n ";
+        cout << "2. Log-in\n";
+        cout << "3. About us \n";
+        cout << "Enter your option number :";
+        cin >> x;
+
+        if(x==1)
+        {
+            cout<< "Registration\n";
             break;
 
         }
         else if(x==2){
-            cout << "Log-in"<< endl;
+            cout<< "Log-in\n";
             break;
-
         }
         else if(x==3){
-            cout<< " about us " <<endl;
+            cout<<"about us \n";
             break;
-
+        }
+        else{
+            cout<<"try again,input is not correct\n";
+             
         }
 
-    
-    }
-
-    
-  
-    
+    }while(x<4 || x>0);
 
 }
