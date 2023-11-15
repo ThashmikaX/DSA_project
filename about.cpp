988ee29facd61c42e5e973eb7ac5fdd8ef22229f
viraj <vchampike@gmail.com>
2023-11-15 09:50:40 +0530

    about_us page is done 15/11

---
+++ about.cpp
@@ -0,0 +1,9 @@
+// aboutus.cpp
+#include "about.h"
+#include <iostream>
+using namespace std;
+
+void aboutPage() {
+    cout << "Welcome to the About Us Page!\n";
+    // Add information about your team, project, etc.
+}
