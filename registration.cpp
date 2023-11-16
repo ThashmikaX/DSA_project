47b0dce172ef1ebb08ccb2be3460b8b882120546
Menura Dulkith <142730909+Menuradulkith@users.noreply.github.com>
2023-11-16 10:32:10 +0530

    update registration.cpp

--- registration.cpp
+++ registration.cpp
@@ -23,22 +23,36 @@ std::string * registration()
         std::cin >> person[4];
         std::cout << "Username : ";
         std::cin >> person[5];
-        std::cout << "Create your login password" << std::endl;
-        std::cout << "Password : ";
+    do {
+        std::cout << "Create your login password (at least 8 characters, no commas): ";
         std::cin >> person[6];
-        while (person[6].length() < 8) {
+
+     
+        if (person[6].length() < 8) {
             std::cout << "Please enter a strong password (at least 8 characters)" << std::endl;
-            std::cout << "Password : ";
-            std::cin >> person[6];
+            continue;
         }
 
-        std::cout << "Confirm your password" << std::endl;
-        std::cin >> confirm_p;
-        while (person[6] != confirm_p) {
-            std::cout << "Password does not match. Please try again." << std::endl;
-            std::cout << "Confirm your password" << std::endl;
-            std::cin >> confirm_p;
+       
+        if (person[6].find(',') != std::string::npos) {
+            std::cout << "Please enter valid characters." << std::endl;
+            continue;
         }
+
+        break; 
+
+    } while (true);
+
+   
+    do {
+        std::cout << "Confirm your password: ";
+        std::cin >> confirm_p;
+
+        if (person[6] != confirm_p) {
+            std::cout << "Password does not match. Please try again." << std::endl;
+        }
+
+    } while (person[6] != confirm_p);
 
         std::cout << "\n" << "Signup successful! Thank you for joining the Banking App." << std::endl;
 
