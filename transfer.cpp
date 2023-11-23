5cbfe0464d68a82cf9fb9e77d2d77b23ce13dfe3
viraj <vchampike@gmail.com>
2023-11-23 17:02:29 +0530

    transfer.cpp

--- transfer.cpp
+++ transfer.cpp
@@ -2,10 +2,34 @@
 #include <iostream>
 #include <limits> 
 #include <string>
+#include "API_v2.h"
+#include "class.h"
 using namespace std;
 
 void transferPage(){
-    int amount;
     
+    float amount;
+    string x;
+    cout << "------Enter Acount Number-------\n";
+    cout <<"To : ";
+    cin >> x;
+    amount =0.0;
+    if(!personList1.updateBalance(x,amount)){
+        cout << "Invalid Account number\n";
+
+    }
+    else{
+        cout << "Enter Amount : ";
+        cin >> amount;
+        personList1.updateBalance(x,amount);
+        cout << "Succefully Transfer\n";
+        cout << "-----Thanks for using !--------\n";
+    }
+
+   
+   
+    
+
+
     cout<<"Account Balance is 10000$";
 }
